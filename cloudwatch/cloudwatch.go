49c08881163640c5212bdf63433eb3f4dc08853c
Simon Thulbourn <simon+github@thulbourn.com>
2016-06-11 22:03:46 +0100

    Remove detail interface
    Instead, we store the detail as json.RawMessage and expect the user to
    unmarshal it

--- cloudwatch/cloudwatch.go
+++ cloudwatch/cloudwatch.go
@@ -8,72 +8,16 @@ import (
 	"github.com/apex/go-apex"
 )
 
-// var (
-// 	detailTypes = map[string]
-// )
-
 // Event represents a CloudWatch Event
 type Event struct {
-	ID         string    `json:"id"`
-	DetailType string    `json:"detail-type"`
-	Source     string    `json:"source"`
-	Account    string    `json:"account"`
-	Time       time.Time `json:"time"`
-	Region     string    `json:"region"`
-	Resources  []string  `json:"resources"`
-	Detail     Detail    `json:"detail"`
-}
-
-// UnmarshalJSON extracts raw data into structured data
-func (e *Event) UnmarshalJSON(b []byte) error {
-	var aux struct {
-		ID         string          `json:"id"`
-		DetailType string          `json:"detail-type"`
-		Source     string          `json:"source"`
-		Account    string          `json:"account"`
-		Time       time.Time       `json:"time"`
-		Region     string          `json:"region"`
-		Resources  []string        `json:"resources"`
-		Detail     json.RawMessage `json:"detail"`
-	}
-
-	if err := json.Unmarshal(b, &aux); err != nil {
-		return err
-	}
-
-	e.ID = aux.ID
-	e.DetailType = aux.DetailType
-	e.Source = aux.Source
-	e.Account = aux.Account
-	e.Time = aux.Time
-	e.Region = aux.Region
-	e.Resources = aux.Resources
-
-	var detail Detail
-	switch aux.Source {
-	case "aws.autoscaling":
-		detail = &AutoScalingGroupDetail{}
-	case "aws.signin":
-		detail = &ConsoleSignInDetail{}
-	case "aws.ec2":
-		detail = &EC2Detail{}
-	default:
-		detail = &EmptyDetail{}
-	}
-
-	if err := json.Unmarshal(aux.Detail, detail); err != nil {
-		return err
-	}
-
-	e.Detail = detail
-
-	return nil
-}
-
-// Detail represents specifics of the event
-type Detail interface {
-	List() []string
-	Get(key string) (interface{}, bool)
+	ID         string          `json:"id"`
+	DetailType string          `json:"detail-type"`
+	Source     string          `json:"source"`
+	Account    string          `json:"account"`
+	Time       time.Time       `json:"time"`
+	Region     string          `json:"region"`
+	Resources  []string        `json:"resources"`
+	Detail     json.RawMessage `json:"detail"`
 }
 
 // AutoScalingGroupDetail of the triggered event
@@ -90,30 +34,10 @@ type AutoScalingGroupDetail struct {
 	EndTime   time.Time `json:"EndTime"`
 }
 
-// List struct fields in the details
-func (d *AutoScalingGroupDetail) List() []string {
-	return listKeys(d)
-}
-
-// Get a value from details based on the key
-func (d *AutoScalingGroupDetail) Get(key string) (interface{}, bool) {
-	return keyValue(d, key)
-}
-
 // EC2Detail of the triggered event
 type EC2Detail struct {
 	InstanceID string `json:"instance-id"`
 	State      string `json:"state"`
-}
-
-// List struct fields in the details
-func (d *EC2Detail) List() []string {
-	return listKeys(d)
-}
-
-// Get a value from details based on the key
-func (d *EC2Detail) Get(key string) (interface{}, bool) {
-	return keyValue(d, key)
 }
 
 // ConsoleSignInDetail of the triggered event
@@ -133,30 +57,6 @@ type ConsoleSignInDetail struct {
 	SourceIPAddress     string            `json:"sourceIPAddress"`
 	UserAgent           string            `json:"userAgent"`
 	UserIdentity        map[string]string `json:"userIdentity"`
-}
-
-// List struct fields in the details
-func (d *ConsoleSignInDetail) List() []string {
-	return listKeys(d)
-}
-
-// Get a value from details based on the key
-func (d *ConsoleSignInDetail) Get(key string) (interface{}, bool) {
-	return keyValue(d, key)
-}
-
-// EmptyDetail of the triggered event
-// This is useful for unknown or schedule event types
-type EmptyDetail struct{}
-
-// List struct fields in the details
-func (d *EmptyDetail) List() []string {
-	return listKeys(d)
-}
-
-// Get a value from details based on the key
-func (d *EmptyDetail) Get(key string) (interface{}, bool) {
-	return keyValue(d, key)
 }
 
 // Handler handles CloudWatch Events
