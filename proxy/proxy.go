629ecdae2f00d7ea69d54cfdfe1b7d0733cfa382
TJ Holowaychuk <tj@vision-media.ca>
2017-03-15 10:29:49 -0700

    refactor proxy handler slightly

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -8,39 +8,34 @@ import (
 	"github.com/apex/go-apex"
 )
 
-// Serve adaptes an http.Handler to the apex.Handler interface
-func Serve(handler http.Handler) *Handler {
-	return &Handler{handler}
+// Serve adaptes an http.Handler to the apex.Handler interface.
+func Serve(h http.Handler) apex.Handler {
+	return &handler{h}
 }
 
 // Handler implements the apex.Handler interface and adapts it to an
 // http.Handler by converting the incoming event to an http.Request object
-type Handler struct {
+type handler struct {
 	Handler http.Handler
 }
 
 // Handle accepts a request from the apex shim and dispatches it to an http.Handler
-func (p *Handler) Handle(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-
-	// Parse the incoming proxy event from API Gateway
+func (p *handler) Handle(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 	proxyEvent := &Event{}
+
 	err := json.Unmarshal(event, proxyEvent)
 	if err != nil {
 		return nil, fmt.Errorf("Parse proxy event: %s", err)
 	}
 
-	// Build an http.Request from the evnt
-	request, err := buildRequest(proxyEvent, ctx)
+	req, err := buildRequest(proxyEvent, ctx)
 	if err != nil {
 		return nil, fmt.Errorf("Build request: %s", err)
 	}
-	responseWriter := &ResponseWriter{}
 
-	// Handle the request
-	p.Handler.ServeHTTP(responseWriter, request)
+	res := &ResponseWriter{}
+	p.Handler.ServeHTTP(res, req)
+	res.finish()
 
-	// Finish writing the response
-	responseWriter.finish()
-
-	return &responseWriter.response, nil
+	return &res.response, nil
 }
