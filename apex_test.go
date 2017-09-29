4a9f27263ec2fda6d10c900c107c6007fb75302c
TJ Holowaychuk <tj@vision-media.ca>
2017-09-29 16:28:42 +0300

    add shim test and benchmark

--- apex_test.go
+++ apex_test.go
@@ -1,18 +1,82 @@
 package apex
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
+	"sync"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/tj/assert"
 )
 
-// HandlerFunc Handler assertion.
-var _ Handler = HandlerFunc(func(event json.RawMessage, ctx *Context) (interface{}, error) {
-	return nil, nil
-})
+var eventInput = `{
+	"event": {
+		"foo": "bar"
+	},
+	"context": {
+		"invokeid": "test"
+	}
+}`
 
-func Test(t *testing.T) {
-	assert.Nil(t, nil)
-	// TODO: shim tests
+func TestHandler(t *testing.T) {
+	var wg sync.WaitGroup
+
+	n := 50
+	wg.Add(n)
+
+	var events []string
+	var contexts []string
+	h := HandlerFunc(func(event json.RawMessage, ctx *Context) (interface{}, error) {
+		events = append(events, string(event))
+		contexts = append(contexts, ctx.InvokeID)
+		wg.Done()
+		return nil, nil
+	})
+
+	var buf bytes.Buffer
+	pr, pw := io.Pipe()
+
+	m := &manager{
+		Reader:  pr,
+		Writer:  &buf,
+		Handler: h,
+	}
+
+	go m.Start()
+
+	go func() {
+		for i := 0; i < n; i++ {
+			pw.Write([]byte(eventInput))
+		}
+		pw.Close()
+	}()
+
+	wg.Wait()
+
+	for i, e := range events {
+		assert.Equal(t, "{\n\t\t\"foo\": \"bar\"\n\t}", e)
+		assert.Equal(t, "test", contexts[i])
+	}
+}
+
+func BenchmarkHandler(b *testing.B) {
+	h := HandlerFunc(func(event json.RawMessage, ctx *Context) (interface{}, error) {
+		return nil, nil
+	})
+
+	var buf bytes.Buffer
+	pr, pw := io.Pipe()
+
+	m := &manager{
+		Reader:  pr,
+		Writer:  &buf,
+		Handler: h,
+	}
+
+	go m.Start()
+
+	for i := 0; i < b.N; i++ {
+		pw.Write([]byte(eventInput))
+	}
 }
